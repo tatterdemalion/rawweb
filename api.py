992c5e1a2b88309c778658b95023175983d6867e
Can Mustafa Özdemir <canmustafaozdemir@gmail.com>
2015-07-23 19:55:20 +0300

    run over network

--- api.py
+++ api.py
@@ -185,4 +185,4 @@ def index():
     return render_template('index.html', path=path, media_host=media_host)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
