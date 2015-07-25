83ce8b51f50f09d2a3234db382a53224d226b125
Can Mustafa Özdemir <canmustafaozdemir@gmail.com>
2015-07-25 03:40:38 +0300

    dropzone events

--- static/js/api.js
+++ static/js/api.js
@@ -3,29 +3,31 @@ var folderTemplateSource = $("#folder-template").html();
 var fileTemplate = Handlebars.compile(fileTemplateSource);
 var folderTemplate = Handlebars.compile(folderTemplateSource);
 
-var $rows = $('.rows');
-var opts = {scale: 5.25 }
-var spinner = new Spinner(opts).spin();
-$rows.append(spinner.el);
-
-$.getJSON('/api/' + window.location.search, function(data){
-  var paths = data['results']['paths'];
-  var i,j,temparray,chunk = 3;
-  for (i=0, j=paths.length; i<j; i+=chunk) {
-    temppaths = paths.slice(i,i+chunk);
-    var div = '<div class="row">';
-    for (var k=0; k<temppaths.length; k++) {
-      var path = temppaths[k];
-      if(path.pathtype == 'directory') {
-        div += folderTemplate(path);
+var reloadDirectory = function(){
+  var $rows = $('.rows');
+  var opts = {scale: 5.25 }
+  var spinner = new Spinner(opts).spin();
+  $rows.append(spinner.el);
+  $.getJSON('/api/' + window.location.search, function(data){
+    var paths = data['results']['paths'];
+    var i,j,temparray,chunk = 3;
+    for (i=0, j=paths.length; i<j; i+=chunk) {
+      temppaths = paths.slice(i,i+chunk);
+      var div = '<div class="row">';
+      for (var k=0; k<temppaths.length; k++) {
+        var path = temppaths[k];
+        if(path.pathtype == 'directory') {
+          div += folderTemplate(path);
+        }
+        else if (path.pathtype == 'file') {
+          div += fileTemplate(path);
+        }  
       }
-      else if (path.pathtype == 'file') {
-        div += fileTemplate(path);
-      }  
+      div += '</div>';
+      $rows.append(div);
     }
-    div += '</div>';
-    $rows.append(div);
-  }
-  $('.spinner').remove();
-});
+    $('.spinner').remove();
+  });
+}
 
+reloadDirectory();
--- static/js/dropzone_options.js
+++ static/js/dropzone_options.js
@@ -1,15 +1,26 @@
-Dropzone.options.upload = {
-  method: "put",
-  paramName: "image",
-  maxFilesize: 30, // MB
-  accept: function(file, done) {
-    var ext = file.name.split('.').pop();
-    if (ext == "NEF" || ext == "SRW") {
-      done();
+$(document).ready(function(){
+  Dropzone.options.upload = {
+    method: "put",
+    paramName: "image",
+    maxFilesize: 30, // MB
+    accept: function(file, done) {
+      var ext = file.name.split('.').pop();
+      if (ext == "NEF" || ext == "SRW") {
+        done();
+      }
+      else {  
+        done("Please upload only supported raw images. (NEF, SRW)");
+      }
     }
-    else {  
-      done("Please upload only supported raw images. (NEF, SRW)");
-    }
-  }
-};
+  };
 
+  Dropzone.autoDiscover = false;
+
+  $(function() {
+    var uploadDropzone = new Dropzone("#upload");
+    uploadDropzone.on("queuecomplete", function(file) {
+      $('.rows .row').remove();
+      reloadDirectory();
+    });
+  });
+});
